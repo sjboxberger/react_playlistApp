2dd9c6f243c72db11c1e3e7f059ae81ba6513b5d
Seth <sjboxberger@gmail.com>
2025-02-05 05:10:20 -0600

    Adding project header

---
+++ playlist-app/src/comps/presentationals/ProjectHead.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ProjectHead(){
+    return(
+        <div>
+            <h1>Make a Playlist on Apple Music!</h1>
+            <p>Enter serch criteria, add you favorite songs, and upload to your Apple Music Profile!</p>
+        </div>
+    )
+};
+
+export default ProjectHead;
